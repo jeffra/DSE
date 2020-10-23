cc85ddf7fc14362517f3e9fdcf2cf4d669954fff
Shaden Smith <shaden.smith@microsoft.com>
2020-10-23 23:00:25 +0000

    seed

--- cifar/cifar10_tutorial.py
+++ cifar/cifar10_tutorial.py
@@ -6,6 +6,28 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+import argparse
+
+def get_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description='CIFAR')
+    parser.add_argument('-e',
+                        '--epochs',
+                        default=2,
+                        type=int,
+                        help='number of training epochs')
+    parser.add_argument('--seed',
+                        default=1138,
+                        type=int,
+                        help='random seed')
+    args = parser.parse_args()
+
+    return args
+
+args = get_args()
+
+torch.manual_seed(args.seed)
+
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -61,8 +83,7 @@ net.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(1):  # loop over the dataset multiple times
-
+for epoch in range(args.epochs):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
@@ -115,28 +136,3 @@ with torch.no_grad():
 
 print('Accuracy of the network on the 10000 test images: %d %%' %
       (100 * correct / total))
-
-########################################################################
-# That looks way better than chance, which is 10% accuracy (randomly picking
-# a class out of 10 classes).
-# Seems like the network learnt something.
-#
-# Hmmm, what are the classes that performed well, and the classes that did
-# not perform well:
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' %
-          (classes[i], 100 * class_correct[i] / class_total[i]))
